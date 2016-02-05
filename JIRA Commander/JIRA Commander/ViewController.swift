29489b5a0e37c4f3e145573d775cc56cd057e302
Ordenewitz, Tim <tim.ordenewitz@scandio.de>
2016-02-05 12:25:34 +0100

    Added Login Screen, gitignore

--- JIRA Commander/JIRA Commander/ViewController.swift
+++ JIRA Commander/JIRA Commander/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var EmailTextField: UITextField!
+    @IBOutlet var PWTextField: UITextField!
+    @IBOutlet var ServerAdressTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        EmailTextField.attributedPlaceholder = NSAttributedString(string:"EMAIL",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        EmailTextField.delegate = self
+        
+        PWTextField.attributedPlaceholder = NSAttributedString(string:"PASSWORD",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        PWTextField.delegate = self
+
+        ServerAdressTextField.attributedPlaceholder = NSAttributedString(string:"SERVER ADRESS",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        ServerAdressTextField.delegate = self
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
-
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        UIApplication.sharedApplication().sendAction("resignFirstResponder", to:nil, from:nil, forEvent:nil)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
