1fcb3b781c67ec302ec36d60a2b55a783e8e5399
Ordenewitz, Tim <tim.ordenewitz@scandio.de>
2016-02-05 11:23:11 +0100

    init Commit

---
+++ JIRA Commander/JIRA Commander/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JIRA Commander
+//
+//  Created by Tim Ordenewitz on 05.02.16.
+//  Copyright © 2016 Tim Ordenewitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
