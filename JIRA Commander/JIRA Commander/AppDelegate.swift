220aa252f2a8b5affc985533b9a27008e1db13cc
Ordenewitz, Tim <tim.ordenewitz@scandio.de>
2016-02-26 13:03:26 +0100

    Added AppSee framework

--- JIRA Commander/JIRA Commander/AppDelegate.swift
+++ JIRA Commander/JIRA Commander/AppDelegate.swift
@@ -9,6 +9,8 @@
 import UIKit
 import Fabric
 import Crashlytics
+import Appsee
+
 
 
 @UIApplicationMain
@@ -18,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.    UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: .Default)
         Fabric.sharedSDK().debug = true
-        Fabric.with([Answers.self, Crashlytics.self])
+        Fabric.with([Crashlytics.self, Appsee.self])
         return true
     }
 
