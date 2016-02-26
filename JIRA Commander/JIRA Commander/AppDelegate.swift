2f8ecc56777515581e2cdb417c69e880b8e41edf
Ordenewitz, Tim <tim.ordenewitz@scandio.de>
2016-02-26 15:17:21 +0100

    Added TableCellActions

--- JIRA Commander/JIRA Commander/AppDelegate.swift
+++ JIRA Commander/JIRA Commander/AppDelegate.swift
@@ -11,15 +11,12 @@ import Fabric
 import Crashlytics
 import Appsee
 
-
-
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.    UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: .Default)
-        Fabric.sharedSDK().debug = true
         Fabric.with([Crashlytics.self, Appsee.self])
         return true
     }
